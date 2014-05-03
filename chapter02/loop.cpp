085488b76f78ac06fa4ca02344eaff2f8251350d
Jason K. Moore <moorepants@gmail.com>
2014-05-03 17:23:58 -0400

    Loop version works.

--- chapter02/loop.cpp
+++ chapter02/loop.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
 #include <string>
 
+using std::cout;
+
 int main()
 {
-	std::cout << "Please enter your first name: ";
+	cout << "Please enter your first name: ";
 	std::string name;
 	std::cin >> name;
+
 	const std::string greeting = "Hello, " + name + "!";
-
 	const int pad = 1;
-	const int rows = pad * 2 + 3
+	const int rows = pad * 2 + 3;
+	const std::string::size_type cols = greeting.size() + pad * 2 + 2;
 
-	std::cout << std::endl;
+	cout << std::endl;
+
 	int r = 0;
 
 	while (r != rows) {
-		std::cout << std::endl;
-		++r;
-	}
-
-	const std::string::size_type cols = greeting.size() + pad * 2 + 2;
-
-	std::string::size_type c = 0;
-
-	while (c != cols) {
-		if (r == 0 || r == rows - 1 || c == 0 || c == cols -1) {
-			std::cout << "*";
-			++c;
-		} else {
-
+		std::string::size_type c = 0;
+		while (c != cols) {
+			if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1) {
+				cout << "*";
+				++c;
+			} else {
+				if (r == pad + 1 && c == pad + 1) {
+					cout << greeting;
+					c += greeting.size();
+				} else {
+					cout << " ";
+					++c;
+				}
+			}
 		}
+		cout << std::endl;
+		++r;
 	}
 
 	return 0;
